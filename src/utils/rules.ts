bd5ac840934c08711d00709f0f4b0af78b93cee6
Seva <vsev.diachun2002@gmail.com>
2023-03-08 23:19:40 +0200

    fix-rules-date-validator

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,5 +1,4 @@
 import moment, {Moment} from "moment";
-import dayjs, {Dayjs} from 'dayjs';
 
 export const rules = {
     required: (message: string = 'Mandatory field!') => ({
@@ -7,8 +6,10 @@ export const rules = {
         message
     }),
     isDateAfter: (message: string = "You can't create events in the past") => ({
-        validator(_: any, value: Dayjs) {
-            if (value.isAfter(dayjs())) {
+        validator(_: any, value: Moment) {
+            const valueDate = value.format('DD.MM.YYYY HH:mm');
+            const momentDate = moment().format('DD.MM.YYYY HH:mm');
+            if (moment(valueDate).isSameOrAfter(momentDate)) {
                 return Promise.resolve()
             }
             return Promise.reject(new Error(message));
